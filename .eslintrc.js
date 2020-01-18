d4076d05a5bfc1ae4fe4d51b04b42a02258584ff
Ivan Chirkin <chirkin.ivan@gmail.com>
2020-01-18 16:52:09 +0300

    chore: change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
     'no-console': [0],
     '@typescript-eslint/member-delimiter-style': [0],
     '@typescript-eslint/camelcase': [0],
+    '@typescript-eslint/no-use-before-define': [0],
     '@typescript-eslint/no-var-requires': [0],
     '@typescript-eslint/explicit-function-return-type': [0],
     'prettier/prettier': 'error',
